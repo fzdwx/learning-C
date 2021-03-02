6e15f9e9323b396346af368ab6842e5cd4dd7a29
like <980650920@qq.com>
2021-03-02 18:44:23 +0800

    break语句

---
+++ day1/demo-3.c
@@ -0,0 +1,37 @@
+/*****************************
+ * @Createdby likelove
+ * @contact  980650920@qq.com
+ * @date 2021/3/2
+ * @desc ${desc}
+ ******************************/
+#include <stdio.h>
+
+int main() {
+    int day = 0;
+    scanf("%d", &day);
+
+    switch (day) {
+        case 1:
+            printf("星期1");
+            break;
+        case 2:
+            printf("星期2");
+            break;
+        case 3:
+            printf("星期3");
+            break;
+        case 4:
+            printf("星期4");
+            break;
+        case 5:
+            printf("星期5");
+            break;
+        case 6:
+            printf("星期6");
+            break;
+        case 7:
+            printf("星期7");
+            break;
+    }
+    return 0;
+}
