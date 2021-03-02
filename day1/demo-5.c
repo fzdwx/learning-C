c1f2080f76008ea3d4b7eeb21dd55bc5bdd99e2a
like <980650920@qq.com>
2021-03-02 19:08:13 +0800

    循环语句

---
+++ day1/demo-5.c
@@ -0,0 +1,16 @@
+/*****************************
+ * @Createdby likelove
+ * @contact  980650920@qq.com
+ * @date 2021/3/2
+ * @desc ${desc}
+ ******************************/
+#include <stdio.h>
+
+int main() {
+    int i = 0;
+    do {
+        i++;
+        printf("%d",i);
+    } while (i < 10);
+    return 0;
+}
