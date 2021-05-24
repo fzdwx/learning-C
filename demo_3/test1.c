ac2b7d257f8a39150a3e662e8c147bf0f90a3bcf
like <980650920@qq.com>
2021-05-24 20:13:46 +0800

    update

---
+++ demo_3/test1.c
@@ -0,0 +1,18 @@
+/*****************************
+ * @Createdby likelove
+ * @contact  980650920@qq.com
+ * @date 2021/5/22
+ * @desc ${desc}
+ ******************************/
+
+#include <stdio.h>
+
+
+int main() {
+    int arr[] = {
+            1, 2, 3, 5, 6, 7
+    };
+    for (int i = 1; i <= 100; i++) {
+        printf("%d\n", i);
+    }
+}
---
+++ demo_4/cmake-build-debug/CMakeFiles/demo_4.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/demo_4.dir/main.c.obj"
+  "demo_4.exe"
+  "demo_4.exe.manifest"
+  "demo_4.pdb"
+  "libdemo_4.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/demo_4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
